d72d57b18cb6729f99eb35f038b8a74cae8c1ae2
ShadySeko <sergeylycee@gmail.com>
2023-10-04 13:48:22 +0200

    added a new command, replace

--- MyCli/src/main/java/HEIG/ReverseWords.java
+++ MyCli/src/main/java/HEIG/ReverseWords.java
@@ -1,5 +1,8 @@
+/**
+ * Author: Sergey Komarov, sergey.komarov@heig-vd.ch
+ * Course: DAI
+ */
 package HEIG;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -14,9 +17,9 @@ import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 
-@Command (name = "reverse", mixinStandardHelpOptions = true, version = "reverseWords 1.0",
-        description = "Reverses the words of an input file and writes the output to an output file.")
-
+@Command (name = "reverse_replace", mixinStandardHelpOptions = true, version = "reverseWords 1.0",
+        description = "Reverses the words of an input file and writes the output to an output file with the reverse command, or " +
+                "replaces all occurences of a character by another in an input file and writes the output to an output file with the replace command.")
 public class ReverseWords implements Callable<Integer> {
 
     /**
@@ -37,24 +40,24 @@ public class ReverseWords implements Callable<Integer> {
 
 
 
-    /**
-     * Overriding the call method of the Callable interface, here we
-     * implement what our command actually does, i.e. takes an input file,
-     * reverses its content and writes it to an output file.
-     * @return 0 if the command was executed successfully, 1 otherwise
-     * @throws Exception
-     */
-    @Override
-    public Integer call() throws Exception {
-        String input = "";
 
-        //Here we check if any of the passed parameters are invalid
+
+    @Command(name = "replace", mixinStandardHelpOptions = true, version = "replace 1.0",
+            description = "Replaces all occurences of a character by another in an input file and writes the output to an output file.")
+    public Integer replace(@Parameters(index = "0", description = "Character to replace") String charToReplace,
+                          @Parameters(index = "1", description = "Character to replace with") String charToReplaceWith) throws IOException {
+
+        if(charToReplace.length() != 1 || charToReplaceWith.length() != 1){
+            System.out.println("Please enter valid characters to replace and replace with.");
+            return 1;
+        }
+
+        String input = "";
         if(inputFile == null && outputFile == null){
             System.out.println("Please enter a valid input and output file, use -h for help.");
             return 1;
         }
 
-        //If there is a valid input file:
         if(inputFile != null && inputFile.exists()) {
             //Read the input file
             BufferedReader reader = new BufferedReader(new FileReader(inputFile, Charset.forName(inputEncoding)));
@@ -67,15 +70,12 @@ public class ReverseWords implements Callable<Integer> {
             }
             reader.close();
             System.out.println("Input file read successfully.");
-
-            //If there is no valid input file:
         }else {
             System.out.println("Please enter a valid input file, use -i option.");
             return 1;
         }
 
-        //Reverse the words of the input file
-        String output = reverseWords(input);
+        String output = replaceChar(input, charToReplace.charAt(0), charToReplaceWith.charAt(0));
 
         //Write the output to the output file using a buffered writer
         //If there is a valid output file:
@@ -85,7 +85,7 @@ public class ReverseWords implements Callable<Integer> {
             writer.close();
             System.out.println("Output file written successfully.");
 
-        //If there is no valid output file,  we write to console
+            //If there is no valid output file,  we write to console
         }else {
             System.out.println("No valid output file specified, printing to console. To avoid this use -o option.\n" + "" +
                     "#####################################################");
@@ -93,6 +93,89 @@ public class ReverseWords implements Callable<Integer> {
         }
         return 0;
     }
+
+    @Command(name = "reverse", mixinStandardHelpOptions = true, version = "reverse 1.0",
+            description = "Reverses the content of an input file and writes the output to an output file.")
+    public Integer reverse() throws IOException {
+            String input = "";
+            if(inputFile == null && outputFile == null){
+                System.out.println("Please enter a valid input and output file, use -h for help.");
+                return 1;
+            }
+
+            if(inputFile != null && inputFile.exists()) {
+                //Read the input file
+                BufferedReader reader = new BufferedReader(new FileReader(inputFile, Charset.forName(inputEncoding)));
+                //Read the input file line by line
+                String line = reader.readLine();
+                input = "";
+                while (line != null) {
+                    input += line + "\n";
+                    line = reader.readLine();
+                }
+                reader.close();
+                System.out.println("Input file read successfully.");
+            }else {
+                System.out.println("Please enter a valid input file, use -i option.");
+                return 1;
+            }
+
+            String output = reverseWords(input);
+
+            //Write the output to the output file using a buffered writer
+            //If there is a valid output file:
+            if(outputFile != null) {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, Charset.forName(outputEncoding))) ;
+                writer.write(output);
+                writer.close();
+                System.out.println("Output file written successfully.");
+
+                //If there is no valid output file,  we write to console
+            }else {
+                System.out.println("No valid output file specified, printing to console. To avoid this use -o option.\n" + "" +
+                        "#####################################################");
+                System.out.println(output);
+            }
+            return 0;
+    }
+
+
+
+
+    /**
+     * Overriding the call method of the Callable interface, here we
+     * implement what our command actually does, i.e. takes an input file,
+     * reverses its content and writes it to an output file.
+     * @return 0 if the command was executed successfully, 1 otherwise
+     * @throws Exception
+     */
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Please specify a valid command, use -h for help.");
+        return 0;
+    }
+
+
+    /**
+     * This method replaces all occurences of a character by another in a given text
+     * @param text the text to replace characters in
+     * @param charToReplace the character to replace
+     * @param charToReplaceWith the character to replace with
+     * @return output, the text with the characters replaced
+     */
+    private String replaceChar(String text, char charToReplace, char charToReplaceWith) {
+        String output = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == charToReplace) {
+                output += charToReplaceWith;
+            } else {
+                output += text.charAt(i);
+            }
+        }
+        return output;
+    }
+
+
 
     /**
      * This method reverses a given word
@@ -129,6 +212,4 @@ public class ReverseWords implements Callable<Integer> {
         System.out.println("Command exited with exit code: " + exitCode +", executed in " + ((int) System.currentTimeMillis() - startTime) + "ms.");
         System.exit(exitCode);
     }
-
-
 }
